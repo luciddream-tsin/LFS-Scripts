a605660e0610a2d2bd7f97b6895dea80372ecf42
luciddream-tsin <92364028+luciddream-tsin@users.noreply.github.com>
2023-07-17 20:06:43 +0800

    Create 06-adding-lfs-user.sh

---
+++ 06-adding-lfs-user.sh
@@ -0,0 +1,20 @@
+# Create a new group named lfs
+groupadd lfs
+
+# Create a new user named lfs to the lfs group, with default shell bash and home dir.
+useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+
+# Give lfs a password
+passwd lfs
+
+# Grant user lfs to access $LFS
+chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
+case $(uname -m) in
+    x86_64) chown -v lfs $LFS/lib64 ;;
+esac
+
+su - lfs
+
+
+
+
